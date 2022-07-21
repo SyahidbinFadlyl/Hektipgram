bf37af73ea53e8a11995c6fef4db63adf8564d43
SyahidbinFadlyl <ava.rageman18@gmail.com>
2022-07-21 15:45:24 +0700

    syahid

--- router/index.js
+++ router/index.js
@@ -6,6 +6,17 @@ router.post("/", Controller.postLogin)
 
 router.get("/register", Controller.register)
 router.post("/register", Controller.postRegister)
+
+router.use((req, res, next) => {
+  console.log(req.session);
+  if(!req.session.userId) {
+    const err = "LOGIN DULU!"
+    return res.redirect((`/?err=${err}`))
+  } else {
+    next()
+  }
+})
+
 router.get("/home", Controller.home)
 router.get("/post", Controller.addPost)
 
--- seeders/20220720132545-userSeeder.js
+++ seeders/20220720132545-userSeeder.js
@@ -1,11 +1,12 @@
 'use strict';
 const fs = require("fs")
-
+const bcrypt = require('bcryptjs')
 module.exports = {
   up (queryInterface, Sequelize) {
     
     let data = JSON.parse(fs.readFileSync("./data/users.json", "utf-8"))
     data.forEach(el => {
+      el.password = bcrypt.hashSync(el.password, bcrypt.genSaltSync(8))
       el.createdAt = new Date()
       el.updatedAt = new Date()
     });
