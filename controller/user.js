921fb4f5b4529e592625f2ac00139ea1aeb79c76
jordxx <danieljordan553@gmail.com>
2022-07-21 13:10:38 +0700

    postLogin

--- controller/user.js
+++ controller/user.js
@@ -1,28 +1,46 @@
 const { User, Profile } = require("../models")
-
+const bcrypt = require('bcryptjs')
 
 class Controller {
-    static login(req, res) {
-        res.render('login')
-    }
-
     static register(req, res) {
         res.render('register')
     }
 
     static postRegister(req, res) {
-        const {fullName, gender, dateOfBirth, userName, email, password} = req.body
-        User.create({userName, email, password})
-        .then(user => {
-            console.log(user.id);
-            return Profile.create({fullName, gender, dateOfBirth, UserId:user.id})
-        })
-        .then(userProfile => {
-            res.send("sukses nambah user")
-        })
-        .catch(err => {
-            res.send(err)
-        })
+        const { fullName, gender, dateOfBirth, userName, email, password } = req.body
+        User.create({ userName, email, password })
+            .then(user => {
+                console.log(user.id);
+                return Profile.create({ fullName, gender, dateOfBirth, UserId: user.id })
+            })
+            .then(userProfile => {
+                res.send("sukses nambah user")
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    }
+
+    static login(req, res) {
+        res.render('login', { error: req.query.err })
+    }
+
+    static postLogin(req, res) {
+        const { email, password } = req.body
+        User.findOne({ where: { email } })
+            .then(user => {
+                if (user) {
+                    if (bcrypt.compareSync(password, user.password)) {
+                        return res.redirect('/home')
+                    } else {
+                        const inv = `invalid credentials`
+                        return res.redirect(`/?err=${inv}`)
+                    }
+                }
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
 
     static home(req, res) {
--- router/index.js
+++ router/index.js
@@ -2,6 +2,8 @@ const Controller = require("../controller/user")
 const router = require("express").Router()
 
 router.get("/", Controller.login)
+router.post("/", Controller.postLogin)
+
 router.get("/register", Controller.register)
 router.post("/register", Controller.postRegister)
 router.get("/home", Controller.home)
